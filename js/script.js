2a778c3ea2c8a3901fd02f1943c9b135be3ea7be
JahidShawn <jahidshawn1914@gmail.com>
2024-12-10 23:23:35 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 // js
-const aboutItems = document.querySelectorAll('.about-item');
+document.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    console.log("Scroll position:", window.scrollY);
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+      console.log("Class 'scrolled' added");
+    } else {
+      navbar.classList.remove("scrolled");
+      console.log("Class 'scrolled' removed");
+    }
+  });
+  
 
+const aboutItems = document.querySelectorAll('.about-item');
 aboutItems.forEach(item => {
     const icon = item.querySelector('.icon');
 
